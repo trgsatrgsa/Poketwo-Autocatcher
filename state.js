900de1ca4a3241b25fe6f4240042cc89e3accb3e
ImmaTrgSa <trgsatrgsa@gmail.com>
2026-01-25 23:09:51 +0800

    update state js

--- state.js
+++ state.js
@@ -5,9 +5,6 @@ module.exports = {
     // Is the bot currently paused?
     isSleeping: false,
 
-    // Temporary list of extra channels added via command
-    tempPrivateChannels: [],
-
     // Stats (optional, for fun display)
     stats: {
         totalCaughtSession: 0,
